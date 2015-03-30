1d18cb50e2dbc77ac62cfe46f1013ac8591721b9
xTEddie <t.eddie@live.com>
2015-03-30 13:52:28 -0400

    Fixed rules, missing #13.

--- rules.pl
+++ rules.pl
@@ -1,5 +1,11 @@
 %% Author: Edward Tran & Peter Boulos
 
+%% Changed the maximum depth of the output list to 100 for testing
+:- set_prolog_flag(toplevel_print_options, [quoted(true), portray(true), max_depth(100), priority(699)]).
+
+%% Include facts.pl to this file
+:- consult(facts).
+
 %% Rules
 
 %% 1.
@@ -20,8 +26,10 @@ is_link(Event, Guard) :- transition(S1, S2 , Event, Guard , _), S1 \== S2.
 %% 6.
 all_superstates(Set) :- findall(Superstate, superstate(Superstate, _), List), list_to_set(List, Set).
 
-%% 7.
-ancestor(Ancestor, Descendant) :- superstate(Ancestor, Descendant).
+%% 7. 
+%% Recursive rule
+ancestor(Ancestor, Descendant) :- superstate(Ancestor, Descendant). %% Base case
+ancestor(Ancestor, Descendant) :- superstate(Ancestor, Intermediate) , superstate(Intermediate, Descendant). %% Parent is also considered as ancestor of the child of its child (grand child)
 
 %% 8.
 inherits_transitions(State, List) :- findall((State, S, Event, Guard, Action), transition(State, S, Event, Guard, Action), List).
@@ -33,13 +41,13 @@ all_states(L) :- findall(State, state(State), L).
 all_init_states(L) :- findall(InitState, initial_state(InitState, _), L).
 
 %% 11.
-get_starting_state(State) :- initial_state(State, _). %%
+get_starting_state(State) :- initial_state(State, _). 
 
 %% 12.
 state_is_reflexive(State) :- transition(State, State, _, _, _).
 
 %% 13.
-graph_is_reflexive(false). %%
+
 
 %% 14.
 get_guards(Set) :- findall(Guard, transition(_, _, _, Guard, _), List), list_to_set(List, Set).
